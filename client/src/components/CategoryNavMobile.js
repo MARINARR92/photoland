c3724fe718d24ae757eac63ef1551e3a62b60b2c
unknown <marinaribeirorodrigues@gmail.com>
2023-10-02 10:38:33 -0300

    first commit

--- client/src/components/CategoryNavMobile.js
+++ client/src/components/CategoryNavMobile.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { FiX } from 'react-icons/fi';
 
-const CategoryNavMobile = () => {
-  return <div>CategoryNavMobile</div>;
+import { Link } from 'react-router-dom';
+
+import useFetch from '../hooks/useFetch';
+
+const CategoryNavMobile = ({ setCatNavMobile }) => {
+  const {data}  = useFetch('/categories');
+  return (
+    <div className='w-full h-full bg-primary p-8'>
+      <div 
+        onClick={() => setCatNavMobile(false)}
+        className='flex justify-end mb-8 cursor-pointer'
+      >
+        <FiX className='text-3xl' />
+      </div>
+      <div className='flex flex-col gap-y-8'>
+        {data?.map((category) => {
+          return (
+            <Link 
+              to={`product/${category.id}`}
+              className='uppercase font-medium' 
+              key={category.id}>
+              {category.attributes.title} Cameras
+            </Link>
+          );
+        })}
+      </div>      
+    </div>
+  );
 };
 
 export default CategoryNavMobile;
